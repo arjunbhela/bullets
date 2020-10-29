c11d59ca0614bda370d0a1b09ab3c7b33825965e
arjunbhela <71056419+arjunbhela@users.noreply.github.com>
2020-10-28 18:05:45 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -30,80 +30,80 @@ function setup() {
   weight3=random(400,1500);
   thick4=random(22,83);
 
-  car1 = createSprite(50,250,50,50);
-  car1.velocityX = speed;
-car1.shapeColor = "white";
-  car2= createSprite(50,150,50,50);
-  car2.velocityX = speed2;
-  car2.shapeColor = "white";
+  bul1 = createSprite(50,250,50,50);
+  bul1.velocityX = speed;
+bul1.shapeColor = "white";
+  bul2= createSprite(50,150,50,50);
+  bul2.velocityX = speed2;
+  bul2.shapeColor = "white";
   wall1 = createSprite(1500,200,100,height/2);
   wall1.shapeColor = color(80,80,80);
 
   wall2 = createSprite(1500,200,100,1600);
   wall2.shapeColor = color(80,80,80);
 
-  car3 = createSprite(50,350,50,50);
-  car3.velocityX = speed3;
-  car3.shapeColor = "white";
+  bul3 = createSprite(50,350,50,50);
+  bul3.velocityX = speed3;
+  bul3.shapeColor = "white";
 
-  car4 = createSprite(50,50,50,50);
-  car4.velocityX = speed1;
-  car4.shapeColor = "white";
+  bul4 = createSprite(50,50,50,50);
+  bul4.velocityX = speed1;
+  bul4.shapeColor = "white";
 }
 function draw() {
   background(0);  
-  if (wall1.x-car1.x < (car1.width+wall1.width)/2) {
-car1.velocityX = 0;
-car1.x = 1425;
+  if (wall1.x-bul1.x < (bul1.width+wall1.width)/2) {
+bul1.velocityX = 0;
+bul1.x = 1425;
 var def = 0.5 * weight * speed * speed/(thick1 * thick1 * thick1)
 if (def>10)  {
-car1.shapeColor=color(255,0,0)
+bul1.shapeColor=color(255,0,0)
 }
 
 if (def<10) {
-car1.shapeColor=color(0,255,0);
+bul1.shapeColor=color(0,255,0);
 }
   }
 
-  if (wall1.x-car2.x < (car2.width+wall1.width)/2) {
-    car2.velocityX = 0;
-    car2.x = 1425;
+  if (wall1.x-bul2.x < (bul2.width+wall1.width)/2) {
+    bul2.velocityX = 0;
+    bul2.x = 1425;
     var def = 0.5 * weight * speed * speed/(thick2 * thick2 * thick2)
     if (def>10)  {
-    car2.shapeColor=color(255,0,0)
+    bul2.shapeColor=color(255,0,0)
     }
     
     
     if (def<10) {
-    car2.shapeColor=color(0,255,0);
+    bul2.shapeColor=color(0,255,0);
     }
       }
   
       
-      if (wall1.x-car3.x < (car3.width+wall1.width)/2) {
-        car3.velocityX = 0;
-        car3.x = 1425;
+      if (wall1.x-bul3.x < (bul3.width+wall1.width)/2) {
+        bul3.velocityX = 0;
+        bul3.x = 1425;
         var def = 0.5 * weight * speed * speed/(thick3 * thick3 * thick3)
         if (def>10)  {
-        car3.shapeColor=color(255,0,0)
+        bul3.shapeColor=color(255,0,0)
         }
         
     
         if (def<10) {
-        car3.shapeColor=color(0,255,0);
+        bul3.shapeColor=color(0,255,0);
         }
           }
 
-       if (wall1.x-car4.x < (car4.width+wall1.width)/2) {
-    car4.velocityX = 0;
-    car4.x = 1425;
+       if (wall1.x-bul4.x < (bul4.width+wall1.width)/2) {
+    bul4.velocityX = 0;
+    bul4.x = 1425;
     var def = 0.5 * weight * speed * speed/(thick4 * thick4 * thick4)
     if (def>10)  {
-    car4.shapeColor=color(255,0,0)
+    bul4.shapeColor=color(255,0,0)
     }
     
     if (def<10) {
-    car4.shapeColor=color(0,255,0);
+    bul4.shapeColor=color(0,255,0);
     }
       }
   drawSprites();
